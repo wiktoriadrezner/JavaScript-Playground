82b49229976d8979363385f5da44d91996bb135c
Wiktoria Drezner <93138210+wiktoriadrezner@users.noreply.github.com>
2022-03-28 19:38:17 +0200

    Translated Bear Emoji

---
+++ homework-emoji-translate.js
@@ -0,0 +1,87 @@
+push();
+translate(50, 50);
+
+// Ears (Large)
+fill("#855C52");
+stroke("#855C52");
+ellipse(100, 100, 110);
+ellipse(300, 100, 110);
+
+// Ears (Medium)
+fill("#B7937A");
+stroke("#B7937A");
+ellipse(100, 100, 90);
+ellipse(300, 100, 90);
+
+// Ears (Small)
+fill("#7D695C");
+stroke("#7D695C");
+ellipse(90, 100, 40);
+ellipse(310, 100, 40);
+
+// Body
+fill("#855C52");
+stroke("#855C52");
+ellipse(200, 200, 300);
+
+// Eyebrows
+fill("#8B7263");
+stroke("#8B7263");
+ellipse(120, 160, 75);
+ellipse(280, 160, 75);
+
+fill("#855C52");
+stroke("#855C52");
+ellipse(120, 177, 100);
+ellipse(280, 177, 100);
+
+//Light Brown Body
+fill("#B7937A");
+stroke("#B7937A");
+ellipse(200, 245, 165);
+
+// Eyes (Large)
+fill("#484D51");
+stroke("#484D51");
+ellipse(120, 180, 80);
+ellipse(280, 180, 80);
+
+// Eyes (Medium)
+fill("#7B8084");
+ellipse(130, 195, 35);
+ellipse(290, 195, 35);
+
+// Eyes (Small)
+fill(255);
+stroke(255);
+ellipse(143, 187, 10);
+ellipse(303, 187, 10);
+
+// Tongue
+fill("#992462");
+stroke("#992462");
+rect(185, 220, 30, 90, 30);
+
+stroke("#B7937A");
+strokeWeight(65);
+line(200, 240, 225, 255);
+line(200, 240, 175, 255);
+line(155, 245, 175, 255);
+line(225, 255, 245, 245);
+
+// Nose and Mouth
+fill("#484D51");
+stroke("#484D51");
+strokeWeight();
+triangle(180, 230, 220, 230, 200, 260);
+strokeWeight(5);
+line(180, 230, 220, 230);
+line(180, 230, 200, 260);
+line(220, 230, 200, 260);
+line(200, 250, 200, 280);
+line(200, 280, 225, 295);
+line(200, 280, 175, 295);
+line(155, 285, 175, 295);
+line(225, 295, 245, 285);
+
+pop();
